f80e412d0f57cbefc60a0854decda57a1646dba4
SofiaMurias <sofiamuriaslopez@gmail.com>
2021-04-30 12:16:36 +0200

    add ejercicies

---
+++ modulo-2/Lección-1/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let direction = 'Calle Rio fuentebrada'
+direction = 'calle piedrahita,22'
+console.log(direction);
+
+const text = document.querySelector('.js-text');
+
+text.innerHTML +=' mundo';
+
+const tittle = document.querySelector('.js-tittle');
+const sofia = document.querySelector('.js-item1');
+const lucia = document.querySelector('.js-item2');
+
+tittle.innerHTML+= ' lucia';
+
+const password = document.querySelector('.password');
+const ada = document.querySelector('.ada');
+ada.innerHTML= '**';
